0b711d3017d2be73bb806fa3b07e9a53c684a45b
AldanaBit <164077208+AldanaBit@users.noreply.github.com>
2025-05-26 18:02:16 -0300

    primer commit del proyecto

---
+++ Clase 9/index.js
@@ -0,0 +1,21 @@
+let entrada = prompt('Ingresa tu nombre')
+
+console.log(entrada)
+let nombre = 'Aldana'
+console.log(nombre)
+
+var nombre2 = "Kelly";
+const PI = 3.14;
+PI = 2;
+console.log(typeof(PI))
+console.log(typeof(nombre))
+
+let nombre3 = 'Miguel';
+console.log(nombre3)
+nombre3 = 4;
+console.log(nombre3)
+
+var numero = prompt("Ingrese un numero")
+console.log(numero)
+let numero = prompt("Ingrese un numero")
+console.log(numero)
