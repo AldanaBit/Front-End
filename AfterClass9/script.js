71ba62b004c4bada8efea6aa4031f19479b06717
AldanaBit <164077208+AldanaBit@users.noreply.github.com>
2025-05-26 21:18:34 -0300

    AfterClass9

---
+++ AfterClass9/script.js
@@ -0,0 +1,11 @@
+//Funcion que bloquea el hilo principal por X milisegundos
+function bloquear(ms){
+    const inicio = Date.now();
+    while (Date.now() - inicio < ms){
+        //espera activa (bloqueo del hilo principal)
+    }
+    console.log("culmina el bloqueo")
+}
+
+//Bloquea el hilo por 3 segundos
+bloquear(6000);
